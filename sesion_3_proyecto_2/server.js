3b1c429acb191468280dab6b4faf91f484bd7df9
Alan Badillo <badillo.soft@hotmail.com>
2018-01-24 13:34:31 -0600

    Sesión 3

--- sesion_3_proyecto_2/server.js
+++ sesion_3_proyecto_2/server.js
@@ -1,17 +1,44 @@
 const http = require("http");
 const express = require("express");
+const nunjucks = require("nunjucks");
 const db = require("./db");
 
 const app = express();
+
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app
+});
+
+app.get("/", (req, res) => {
+    db.get_persons().then(result => {
+        res.render("index.html", {
+            personas: result
+        });
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+app.get("/persona/nueva", (req, res) => {
+    res.render("form_new_person.html");
+});
 
 app.get("/api/persons/new", (req, res) => {
     const nombre = req.query.nombre;
     const edad = Number(req.query.edad);
     const sexo = req.query.sexo;
     const salario = Number(req.query.salario);
+    const redirect = req.query.redirect;
 
     db.insert_person(nombre, edad, sexo, salario).then(result => {
+        if (redirect) {
+            res.redirect(redirect);
+            return;
+        }
+
         res.send(result);
+        // res.redirect("/persona/nueva");
     }).catch(err => {
         res.status(400).send(err);
     });
